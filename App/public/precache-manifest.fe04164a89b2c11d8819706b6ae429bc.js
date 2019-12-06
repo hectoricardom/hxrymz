f1985a9f992ddc1f20eb0e2489d60eac635cd49e
Hector Ricardo <hectoricardom@gmail.com>
2019-12-05 23:04:09 -0500

    sdgosiglskb

---
+++ App/public/precache-manifest.fe04164a89b2c11d8819706b6ae429bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06e397e305dd2f430def",
+    "url": "/static/css/main.44746920.chunk.css"
+  },
+  {
+    "revision": "06e397e305dd2f430def",
+    "url": "/static/js/main.06e397e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "06d550bb9f55e38150e1",
+    "url": "/static/js/2.06d550bb.chunk.js"
+  },
+  {
+    "revision": "882ff0d7a537753c7576da24e2c8461b",
+    "url": "/index.html"
+  }
+];
--- App/public/service-worker.js
+++ App/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32446ac575754377aadeff5d1f4763b0.js"
+  "/precache-manifest.fe04164a89b2c11d8819706b6ae429bc.js"
 );
 
 workbox.clientsClaim();
