1732fc7684ce8cfcc13ad8f033da1436f14c3e6b
Hector Ricardom <hectoricardom@gmail.com>
2020-11-27 22:27:12 -0500

    dfte364634634

--- App/public/service-worker.js
+++ App/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f5031a9569b31988ba5f3aef630f065.js"
+  "/precache-manifest.a27495145a94623cf9afb5fd4c497436.js"
 );
 
 workbox.clientsClaim();
